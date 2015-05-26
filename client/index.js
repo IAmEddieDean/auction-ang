83b055643cc192565c477a0e07bfc57c8c2449e7
Chris Oyler <christopher.oyler@gmail.com>
2015-05-26 00:20:31 -0700

    update readme & scrub pos

--- client/index.js
+++ client/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('poseidon', ['firebase', 'ui.router']);
+angular.module('auction', ['firebase', 'ui.router']);
--- client/views/users/users.js
+++ client/views/users/users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('poseidon')
+angular.module('auction')
 .controller('UsersCtrl', function($scope, $state, $window, User){
   $scope.name = $state.current.name;
 
