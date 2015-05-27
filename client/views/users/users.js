f2d9192d26b964c56aaf875774b69a9a64cf3007
Chris Oyler <christopher.oyler@gmail.com>
2015-05-26 17:21:03 -0700

    user model, login, auth, create working

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -1,27 +1,39 @@
 'use strict';
 
 angular.module('auction')
-.controller('UsersCtrl', function($scope, $state, $window, User, states){
+.controller('UsersCtrl', function($scope, $state, $window, User, states, $rootScope){
   $scope.states = states;
   $scope.submit = function(user){
     User.login(user)
     .then(function(response){
-      console.log(response);
-      // if(response.data){
-      //   User.login(user)
-      //   .then(function(res){
-      //     console.log(res);
-      //     $state.go('dashboard.user', {userId: res.data._id});
-      //   });
-      // }else{
-      //   $state.go('register', user);
-      // }
-    }).catch(function(){
-      $window.swal({title: 'Login Error', text: 'There was a problem with your login. Please try again.', type: 'error'});
+      $rootScope.activeUser = response.data;
+      $state.go('dashboard.user')
+    })
+    .catch(function(res){
+      if(res.status === 418){
+        $state.go('register');
+      }else{
+        $window.swal({title: 'Login Error', text: 'There was a problem with your login. Please try again.', type: 'error'});
+      }
     });
   };
-  
+
   $scope.register = function(user){
-    console.log(user);
+    if(user.password1 !== user.password2){
+      $window.swal({title: 'Registration Error', text: "Your passwords don't match. Please try again.", type: 'error'});
+      $scope.user.password1 = null;
+      $scope.user.password2 = null;
+    }else{
+      user.password = user.password1;
+      delete user.password1;
+      delete user.password2;
+      User.register(user)
+      .then(function(response){
+        $rootScope.activeUser = response.data;
+        $state.go('dashboard.user');
+      }).catch(function(){
+        $window.swal({title: 'Login Error', text: 'There was a problem with your login. Please try again.', type: 'error'});
+      });
+    }
   };
 });
