98dad72ae24cbb6ddf6c32ff2c1eea9c040fc692
Chris Oyler <christopher.oyler@gmail.com>
2015-06-04 09:56:26 -0700

    changed nodeurl

--- client/config/constants.js
+++ client/config/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('auction')
-.constant('nodeUrl', 'https://fathomless-falls-9087.herokuapp.com/')
+.constant('nodeUrl', 'https://fathomless-falls-9087.herokuapp.com')
 .constant('states', ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']);
